cf823dcb6249f8d4d5a9311b4632fa58b63736f5
casawolice <casawolice@qq.com>
2025-08-14 16:59:27 +0800

    feat:update

--- examples/rest-public.ts
+++ examples/rest-public.ts
@@ -1,9 +1,14 @@
 import { RestClient } from '../src/index';
+import {setGloabAgent,getAgent,setAxiosAgent} from "../src/agent"
 
 // or
 // import { RestClient } from 'okx-api';
-
-const client = new RestClient();
+const socket={
+  ip:"127.0.0.1",
+  port:15236
+}
+setGloabAgent(socket)
+const client = new RestClient(null,"demo",{},setAxiosAgent({},socket));
 
 /**
  * This is a simple script wrapped in a immediately invoked function expression, designed to make public API calls without credentials
---
+++ src/agent.ts
@@ -0,0 +1,45 @@
+import https from 'node:https';
+import http from 'node:http';
+import { SocksProxyAgent } from 'socks-proxy-agent';
+import type { AxiosRequestConfig } from 'axios';
+import { HttpProxyAgent } from 'http-proxy-agent';
+import { HttpsProxyAgent } from 'https-proxy-agent';
+
+type SocksAgent = InstanceType<typeof SocksProxyAgent>;
+type Agent= InstanceType<typeof SocksProxyAgent> |  InstanceType<typeof HttpProxyAgent> |  InstanceType<typeof HttpsProxyAgent>;
+interface AgentSocket {
+  ip?: string;
+  port: number;
+}
+
+const getAgent = (agentSocket: AgentSocket,protocol:'http'|'https'|'socks'="http"): Agent => {
+  let { ip, port } = { ip: '127.0.0.1', ...agentSocket };
+  switch(protocol){
+    case "http":
+       return new HttpProxyAgent(`http://${ip}:${port}`); 
+    break;
+     case "https":
+       return new HttpsProxyAgent(`http://${ip}:${port}`); 
+    break;
+     case "socks":
+       return new SocksProxyAgent(`socks://${ip}:${port}`); 
+    break;
+  }
+
+};
+
+const setGloabAgent = (agentSocket: AgentSocket) => {
+
+  http.globalAgent =  getAgent(agentSocket,"http");
+  https.globalAgent =  getAgent(agentSocket,"https");;
+};
+// @ts-ignore
+const setAxiosAgent = (
+  axiosRequestConfig: AxiosRequestConfig,
+  agentSocket: AgentSocket
+) => {
+ 
+  return { ...(axiosRequestConfig || {}), httpAgent: getAgent(agentSocket,"http"), httpsAgent: getAgent(agentSocket,"https") };
+};
+
+export { setGloabAgent, setAxiosAgent, getAgent };
